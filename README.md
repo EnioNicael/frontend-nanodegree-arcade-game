81d3df85396f84124c635902baf7acfc83cb9e07
Enio Nicael <enionicael@gmail.com>
2017-11-04 01:08:08 -0200

    feat: Conclusao para envio
    Conclusao do projeto com as funcionalidades basicas.

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
-frontend-nanodegree-arcade-game
-===============================
+# Front-end Nanodegree Arcade Game
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+## Como rodar o jogo
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+Você pode *clonar* do repositório ou fazer o *download* do github, se fizer o download será necessario descompactar os arquivos e pastas do jogo, então poderá executá-lo em seu navegador web favorito.
+
+## Como jogar
+
+**Movimentação:**
+Para movimentar o jogador é necessario utilizar as teclas de setas direcionais *Left*, *Right*, *Up* e *Down*.
+
+**Instruções do jogo:**
+- O jogador deverá movimentar-se e passar pelos insetos sem que haja colisões até chegar na água.
+- A cada passagem com êxito a pontuação do jogador é incrementada e reinicia na posição de origem, porém se o jogador for atingido pelo inimigo a pontuação é zerada e retorna na posição inicial.
+
+## Licença
+
+O conteúdo deste repositório está licenciado sob uma licença [MIT](https://opensource.org/licenses/MIT).
--- js/app.js
+++ js/app.js
@@ -6,43 +6,104 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.position = //em construção
-    this.speed = //em construção
+    this.x = 0; //posicao em relacao ao eixo x
+    this.y = 0; //posicao em relacao ao eixo y
+    this.speed = 50 + Math.floor(56 * Math.random()); // velocidade do inimigo
+    this.line = Math.floor(3 * Math.random()); // indice aleatorio para o array position
 };
-
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    //atualiza o local do inimigo
+    if(this.x <= 505){ // se objeto nao atingiu o fim da linha, ele é incrementado
+        this.x = (this.x + this.speed * dt);
+    }else { // se chegou ao fim da linha ele volta ao inicio
+        this.reset();
+    }
+    // controla a colisão
+    this.colision();
 };
-
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
+    // array com a posicao das tres linhas onde passam os inimigos
+    var position = [60, 150, 230];
+    // a posicao do inimigo e escolhido aleatoriamente
+    this.y = position[this.line];
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+// reseta as propriedades do objeto
+Enemy.prototype.reset = function(){
+    this.x = 0; // faz com que o objeto retorne ao inicio da linha
+    this.speed = 50 + Math.floor(56 * Math.random()); //velocidade
+    this.line = Math.floor(3 * Math.random()); // indice para o array de linhas dos inimigos
+};
+// controle de colisao
+Enemy.prototype.colision = function(){
+    // dimensoes da imagem
+    var width = 101;
+    var height = 171;
+    // se houver colisao, o jogador é resetado
+    if (this.x + (width - 2) > player.x + 16 && // colisao pela direita
+        this.x + 3  < player.x + (width - 16)  && // colisao pela esq
+        this.y + (height - 26) > player.y + 62 && // colisao por baixo
+        this.y + 78 < player.y + (height - 29)){// colisao pela cima
 
+        player.reset();
+    }
+};
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function(){
+    this.sprite = 'images/char-boy.png';
+    this.x = 200; // posicao do eixo x
+    this.y = 380; // posicao do eixo y
+    this.key = ' '; // controla a tecla acionada
+    this.pontos = 0; // pontuação do jogador
+};
+// reseta as propriedades do objeto
+Player.prototype.reset = function(){
+    this.x = 200; //posicao em relacao ao eixo x
+    this.y = 380; //posicao em relacao ao eixo y
+    this.pontos = 0;
+};
+// atualiza o objeto jogador
+Player.prototype.update = function(){
+    if (this.y >= 20) { // se o jogador nao chegou a agua, segue o jogo
+        if (this.key == 'up') { // move para cima
+            this.y -= 20;
+        }
+        if (this.key == 'down' && this.y <= 410) { // move para baixo
+            this.y += 20;
+        }
+        if (this.key == 'left' && this.x >= 0) { // move para esquerda
+            this.x -= 20;
+        }
+        if (this.key == 'right' && this.x <= 406) { // move para direita
+            this.x += 20;
+        }
 
+        this.key = ' '; // limpa a variavel de tecla pressionada
+    }else { // se o jogador chegou na agua ele ganhou
+        this.pontos++; // soma da pontuaçao
+        // reseta a posicao do jogador
+        this.x = 200;
+        this.y = 380;
+    }
+};
+// desenha na tela
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y); // desenha o objeto
+    ctx.font = '38px serif';
+    ctx.fillText(this.pontos, 20, 90); // desenha o texto de pontuaçao
 };
 
-Player.prototype.update(){
-
+Player.prototype.handleInput = function(event){
+    this.key = event;// a propriedade do objeto recebe a tecla acionada
 };
-
-Player.prototype.render(){
-
-};
-
-Player.prototype.handleInput(){
-
-};
-
-
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
@@ -50,11 +111,9 @@ var bug1 = new Enemy();
 var bug2 = new Enemy();
 var bug3 = new Enemy();
 // inimigos
-var allEnemies = [];
-allEnemies.push(bug1, bug2, bug3);
-//jogador
+var allEnemies = [bug1, bug2, bug3];
+// jogador
 var player = new Player();
-
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
